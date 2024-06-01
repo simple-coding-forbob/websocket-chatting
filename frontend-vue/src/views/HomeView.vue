b29040d2e26593707b9c489e1a8f9b9152d41390
KangTaeGyung <96178104+KangTaeGyung@users.noreply.github.com>
2024-06-01 18:51:54 +0900

    프론트 벡엔드 로직 수정
     로직 수정

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -73,7 +73,7 @@ export default {
           userName: this.userName,
           content: this.message,
         };
-        this.stompClient.send("/receive", JSON.stringify(msg), {});
+        this.stompClient.send("/pub", JSON.stringify(msg), {});
       }
     },
     connect() {
@@ -89,7 +89,7 @@ export default {
           console.log("소켓 연결 성공", frame);
           // 서버의 메시지 전송 endpoint를 구독합니다.
           // 이런형태를 pub sub 구조라고 합니다.
-          this.stompClient.subscribe("/send", (res) => {
+          this.stompClient.subscribe("/sub", (res) => {
             console.log("구독으로 받은 메시지 입니다.", res.body);
 
             // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
